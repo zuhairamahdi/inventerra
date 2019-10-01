a720f7f5e7378b1c4b33189afa50f253dc0f156f
Zuhair Abdulla <zuhairamahdi@gmail.com>
2019-10-01 14:14:09 +0300

    Add beego admin package and add its attribute keys to the app.conf file

--- routers/router.go
+++ routers/router.go
@@ -3,8 +3,10 @@ package routers
 import (
 	"github.com/zuhairamahdi/inventerra/controllers"
 	"github.com/astaxie/beego"
+	"github.com/beego/admin" //admin package
 )
 
 func init() {
+	admin.Run()
     beego.Router("/", &controllers.MainController{})
 }
